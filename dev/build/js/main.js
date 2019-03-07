7554954047eb3c32983a0ae072487a85496882a8
Amanda Christian <agc59@drexel.edu>
2019-03-07 14:30:47 -0500

    Navigation Progress

--- dev/build/js/main.js
+++ dev/build/js/main.js
@@ -16,7 +16,7 @@ var $MiddleLine = $('#MiddleLine');
 var $BottomLine = $('#BottomLine');
 
 var isMenuOut = false;
-$navContainer.on("click", menuActions);
+
 
 
 
@@ -24,23 +24,37 @@ function menuActions(){
 
     console.log("working");
     if(isMenuOut === false){
-        // TweenMax.to($navContainer,0.5,{opacity:0});
         TweenMax.set($navContainer,{display:"block"});
         TweenMax.from($navContainer,0.25,{alpha:0});
+        TweenMax.to([$TopLine,$MiddleLine,$BottomLine],0.25,{stroke:"#ffffff"});
+        // TweenMax.to($TopLine,0.25,{rotation:45,transformOrigin:"center"});
+        // TweenMax.to($BottomLine,0.25,{rotation:-45,transformOrigin:"center"});
         isMenuOut = true;
     }
 
     else{
-        // TweenMax.to($navContainer,0.5,{opacity:0.8});
+        TweenMax.to($navContainer,0.25,{alpha:0, onComplete: setDisplayNone});
+        TweenMax.to([$TopLine,$MiddleLine,$BottomLine],0.25,{stroke:"#000000"});
+        // TweenMax.to($TopLine,0.25,{rotation:0});
+        // TweenMax.to($BottomLine,0.25,{rotation:0});
         isMenuOut = false;
+
+
     }
 }
+
+function setDisplayNone(){
+  console.log("setting");
+  TweenMax.set($navContainer,{display:"none"});
+  TweenMax.set($navContainer,{alpha:0.8});
+}
+
 
 
 $burger.on("click", menuActions);
 
 
-$burger.on("mouseenter", burgerOver);
+// $burger.on("mouseenter", burgerOver);
 // this is the function that will be called when the mouse is rolled over the burger
 
 function burgerOver(){
@@ -56,7 +70,7 @@ function burgerOver(){
 }
 
 
-  $burger.on("mouseleave", burgerOut);
+  // $burger.on("mouseleave", burgerOut);
 
 function burgerOut() {
 
--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -16,57 +16,60 @@ var $MiddleLine = $('#MiddleLine');
 var $BottomLine = $('#BottomLine');
 
 var isMenuOut = false;
-$navContainer.on("click", menuActions);
-
-
 
 function menuActions(){
 
     console.log("working");
     if(isMenuOut === false){
-        // TweenMax.to($navContainer,0.5,{opacity:0});
         TweenMax.set($navContainer,{display:"block"});
         TweenMax.from($navContainer,0.25,{alpha:0});
         isMenuOut = true;
     }
 
     else{
-        // TweenMax.to($navContainer,0.5,{opacity:0.8});
+        TweenMax.to($navContainer,0.5,{opacity:0, onComplete: setDisplayNone});
+        // TweenMax.set($navContainer,{display:"none"});
+        // TweenMax.from($navContainer,0.25,{alpha:1});
+        // TweenMax.to($navContainer,0.25,{alpha:0});
+
         isMenuOut = false;
-        
+
     }
 }
 
+function setDisplayNone(){
+  TweenMax.set($navContainer,{display:"none"});
+}
 
 $burger.on("click", menuActions);
 
 
-$burger.on("mouseenter", burgerOver);
+// $burger.on("mouseenter", burgerOver);
 // this is the function that will be called when the mouse is rolled over the burger
 
-function burgerOver(){
+// function burgerOver(){
+//
+//     if(isMenuOut === true){
+//
+//
+// }
+//   else {
+//
+//
+// }
+// }
 
-    if(isMenuOut === true){
-          TweenMax.to($TopLine,0.5,{rotate:-90});
-
-}
-  else {
-
-
-}
-}
-
-
-  $burger.on("mouseleave", burgerOut);
-
-function burgerOut() {
-
-  if (isMenuOut === true) {
-
-  }
-
-  else{
-
-
-  }
-}
+//
+//   $burger.on("mouseleave", burgerOut);
+//
+// function burgerOut() {
+//
+//   if (isMenuOut === true) {
+//
+//   }
+//
+//   else{
+//
+//
+//   }
+// }
